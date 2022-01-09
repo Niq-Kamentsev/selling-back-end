b68a08a0ee350033b306dc4c07aea5dcdd060245
Leonid Hrytsenko <lethaxxxx@gmail.com>
2022-01-09 21:28:41 +0300

    Improved EmitterService stability. Fix EmitterService async exceptions.

--- src/main/java/com/main/sellplatform/service/EmitterService.java
+++ src/main/java/com/main/sellplatform/service/EmitterService.java
@@ -1,11 +1,12 @@
 package com.main.sellplatform.service;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.MediaType;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -13,7 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 public class EmitterService {
 
 	private static final long EMITTER_TIMEOUT = TimeUnit.MINUTES.toMillis(1);
-	private Map<Long, Map<Long, SseEmitter>> emitters = new HashMap<>();
+	private Map<Long, Map<Long, SseEmitter>> emitters = new ConcurrentHashMap<>();
 
 	public SseEmitter getEmitter(Long userId, Long emitterId) {
 		Map<Long, SseEmitter> userEmitters = emitters.get(userId);
@@ -21,7 +22,7 @@ public class EmitterService {
 		if (userEmitters != null) {
 			emitter = userEmitters.get(emitterId);
 		} else {
-			userEmitters = new HashMap<>();
+			userEmitters = new ConcurrentHashMap<>();
 			emitters.put(userId, userEmitters);
 		}
 		if (emitter == null) {
@@ -30,12 +31,16 @@ public class EmitterService {
 				this.removeEmitter(userId, emitterId);
 				System.out.println("onTimeout: " + userId);
 			});
+			emitter.onError(e -> {
+				this.removeEmitter(userId, emitterId);
+			});
 			userEmitters.put(emitterId, emitter);
 		}
 		return emitter;
 	}
-
-	public synchronized void removeEmitter(Long userId, Long emitterId) {
+	
+	@Async("threadPoolTaskExecutor")
+	public void removeEmitter(Long userId, Long emitterId) {
 		Map<Long, SseEmitter> userEmitters = emitters.get(userId);
 		if (userEmitters != null) {
 			SseEmitter emitter = userEmitters.get(emitterId);
@@ -45,7 +50,8 @@ public class EmitterService {
 			userEmitters.remove(emitterId);
 		}
 	}
-
+	
+	@Async("threadPoolTaskExecutor")
 	public void pushNotification(Long targetUserId, Long senderUserId) {
 		Map<Long, SseEmitter> userEmitters = emitters.get(targetUserId);
 		if (userEmitters != null) {
@@ -54,6 +60,7 @@ public class EmitterService {
 					try {
 						emitter.send(SseEmitter.event().data(senderUserId, MediaType.APPLICATION_JSON));
 					} catch (IOException e) {
+						emitter.completeWithError(e);
 						e.printStackTrace();
 					}
 				}
